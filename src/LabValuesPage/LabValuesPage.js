bc88059e9bb639a402f83947eee1c45da0c11088
Matthew Bowlan <mbowlan@upei.ca>
2020-03-04 10:20:05 -0400

    WIP - LabValuesPage
    Seams to be working but no test data available

--- src/LabValuesPage/LabValuesPage.js
+++ src/LabValuesPage/LabValuesPage.js
@@ -1,10 +1,23 @@
 /* REACT IMPORTS */
 import React from 'react';
+import { Link } from 'react-router-dom';
+/* LOCAL IMPORTS */
+import { apiCall } from '../_helpers';
 
 class LabValuesPage extends React.Component {
-    
+
+    state = {
+        labValueHistory: null,
+    }
+
+    componentDidMount() {
+        apiCall(/medical_labs/, { method: 'GET' })
+            .then(response => this.setState({labValueHistory: response}))
+            .catch(_ => this.setState({labValueHistory: []}));
+    }
+
     render() {
-        return <h1>Coming Soon!</h1>;
+        return <Link props={this.state.labValueHistory} to="/program/lab-values">Lab Values</Link>
     }
 }
 
