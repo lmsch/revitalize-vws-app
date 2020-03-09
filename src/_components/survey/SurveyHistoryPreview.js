7bad535b467763d2f22009f3e0bbd371c749d324
Alfred Uzokwe <31731724+AlfredUzokwe@users.noreply.github.com>
2020-03-09 16:02:13 -0300

    Layout/cleanup
    cleaned out comments and arranged the import layout

--- src/_components/survey/SurveyHistoryPreview.js
+++ src/_components/survey/SurveyHistoryPreview.js
@@ -1,10 +1,14 @@
+/* REACT IMPORTS */
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+/* THIRD PARTY IMPORTS */
+import { 
+    makeStyles,
+    Card,
+    CardActions,
+    CardContent,
+    Button,
+    Typography,
+ } from "@material-ui/core";
 import { Link } from 'react-router-dom';
 import { CardHeader } from "@material-ui/core";
 
@@ -34,7 +38,7 @@ export function historyPreview() {
                 )
             </CardContent>
             <CardActions>
-                <Button size="small" variant="outlined" color="primary" /*Link="/program/survey"*/>
+                <Button size="small" variant="outlined" color="primary">
                     <Link to="/program/survey">See More</Link>
                 </Button>
             </CardActions>
