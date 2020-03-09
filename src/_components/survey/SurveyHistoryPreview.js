d1be9f0426f579e9f34b4dfe509cd3d93e3ab351
Alfred Uzokwe <31731724+AlfredUzokwe@users.noreply.github.com>
2020-03-09 15:56:09 -0300

    Display Data
    Set the call, similar to the survey History call, limited map to 3 values

--- src/_components/survey/SurveyHistoryPreview.js
+++ src/_components/survey/SurveyHistoryPreview.js
@@ -5,35 +5,37 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { CardHeader } from "@material-ui/core";
 
+const surveyHistoryPColumns = [
+    {
+        id: 'name',
+        label: 'Name',
+    },
+]
 
 export function historyPreview() {
-    const classes = this.props;
+    const surveyHistoryP = this.props;
 
     return (
         <Card>
             <CardHeader
-                title ="Survey History Preview"
-            titleTypographyProps={{
-                component: "h1",
-                variant: "h4",
-            }}/>
+                title="Survey History Preview"
+                titleTypographyProps={{
+                    component: "h1",
+                    variant: "h4",
+                }} />
             <CardContent>
-                <Typography variant="h6" component="h2">
-                    Insert last used survey
-        </Typography>
-                <Typography variant="h6" component="h2">
-                    insert 2nd to last survey
-        </Typography>
-                <Typography variant="h6" component="h2">
-                    insert 3nd to last survey
-        </Typography>
+                surveyHistoryP?.slice(0,3).map((survey, i) =>(
+                <Typography variant="h6" align="left" key={i}>
+                    {survey.name}
+                </Typography>
+                )
             </CardContent>
             <CardActions>
                 <Button size="small" variant="outlined" color="primary" /*Link="/program/survey"*/>
-                    <Link to="//program/survey">See More</Link>
+                    <Link to="/program/survey">See More</Link>
                 </Button>
             </CardActions>
         </Card>
